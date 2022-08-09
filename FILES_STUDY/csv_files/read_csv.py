822c3ebbc87d1e08359c5269dbde0348305d6f85
renatamoon <r.cardoso.monteiro1@gmail.com>
2022-08-08 23:31:09 -0300

    adding csv pandas

---
+++ FILES_STUDY/csv_files/read_csv.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+
+df = pd.read_csv(
+    'base.csv',
+    )
+
+df.dropna(inplace=True)
+
+new = df["categoria_4"].str.split(". ", n=1, expand=True)
+
+df["Order"] = new[0]
+df["Categoria"] = new[1]
+
+#order = df["Order"].astype(str).astype(int)
+#categoria = df["Categoria"]
+
+df.drop(columns=["categoria_4"], inplace=True)
+
+print(df)
+
+print("********")
+
+categories = pd.Categorical(
+    df["Categoria"],
+    ['Entre 3000 e 4000', 'Acima de 5000', '< 1200', 'Entre 1200 e 1500', 'Entre 1500 e 2000', 'Entre 2000 e 3000', 'Entre 4000 e 5000'],
+    ordered=True
+)
+
+print(categories)
+
+# df["Categoria"] = df["Categoria"].cat.codes
+#
+# print(df.head())
